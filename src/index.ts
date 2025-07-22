ed1d71a47baecdb5f6c74a476e671c2bc9e5124c
--value=ajchen02 <--value=ajchen01@qq.com>
2025-07-22 21:54:48 +0800

    增加配置说明

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,15 @@ import { img } from './img';
 import FormData from 'form-data';
 import axios from 'axios';
 
-export default class FileManager extends Service
+class FileManager extends Service
 {
   static name: string = 'filemanager';
 
   static usage = `# 配置说明
 
-1. 111
+1. 对，这个就是目前新的图床实现方式（
+2. 你需要启用至少一个 filemanager 的附属插件 来让 adapter-iirose 正常工作
+3. 不知道启用什么？ 试试 在插件商店里搜索 filemanager ！
 `;
 
   constructor(ctx: Context, config: any)
@@ -47,6 +49,18 @@ export default class FileManager extends Service
   FormData = FormData; // 让FormData可以在外部使用
   
 }
+
+namespace FileManager {
+  export interface Config{
+    abab: string;
+  }
+
+  export const Config: Schema<Config> = Schema.object({
+    abab: Schema.string().description('目前这个插件没有设定。这个设定在这里只是为了防止他因为代码里没有配置设定而报错（').default('enjoy your day!'),
+  });
+}
+
+export default FileManager;
 
 declare module 'koishi' {
   interface Context
